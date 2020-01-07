53507147c154be284c1b42eeb4ec9a3e33181d3e
jakep5 <jakepagel1@yahoo.com>
2020-01-07 14:38:29 -0600

    implement sign up page wireframe

--- src/routes/SignUpPage/SignUpPage.js
+++ src/routes/SignUpPage/SignUpPage.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import styles from './styles.module.css';
+import { Link } from 'react-router-dom';
 
 export default class SignUpPage extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+        <nav className={styles.nav} role="navigation">
+            <Link to="/signIn">
+                Sign In
+            </Link>
+        </nav>
+        <main className={styles.main} role="main">
+
+            <section className={styles.signUpWrapper} role="banner">
+                <div role="presentation" className={styles.signUpHolder}>
+                    <form className={styles.signUpForm} id="signUpForm" name="signUpForm">
+
+                        <legend className={styles.signUpLegend}>Sign Up</legend>
+
+                        <label for="signUpUsername">Username:</label>
+                        <input type="text" name="signUpUsername" className={styles.signUpUsername} id="signUpUsername" />
+
+                        <br/>
+
+                        <label for="signInPassword">Password:</label>
+                        <input type="password" name="signUpPassword" className={styles.signUpPassword} id="signUpPassword" />
+
+                        <br />
+
+                        <button type="submit" for="signUpForm" className={styles.signUpButton}>Sign Up</button>
+
+                    </form>
+                </div>
+            </section>
+
+        </main>
+        <footer className={styles.footer} role="content-info">Footer</footer>
+      </>
     )
   }
 }
