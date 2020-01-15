5690d3e60df1919254f3f56d57ab63485b7e9a1b
jakep5 <jakepagel1@yahoo.com>
2020-01-15 15:22:09 -0600

    change api base url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     API_KEY: process.env.REACT_APP_API_KEY,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://cryptic-coast-46513.herokuapp.com/",
     TOKEN_KEY: 'game-galaxy-token-key',
     GAME_API_BASE_URL: process.env.GAME_API_BASE_URL || "https://api-v3.igdb.com",
     GAME_API_KEY: process.env.GAME_API_KEY,
