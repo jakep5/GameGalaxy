bbae0971c5add0202c6b377319c3fe8186435bf6
jakep5 <jakepagel1@yahoo.com>
2020-01-15 15:49:05 -0600

    fix bug in env variables

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     API_KEY: process.env.REACT_APP_API_KEY,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://cryptic-coast-46513.herokuapp.com/",
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://cryptic-coast-46513.herokuapp.com",
     TOKEN_KEY: 'game-galaxy-token-key',
     GAME_API_BASE_URL: process.env.GAME_API_BASE_URL || "https://api-v3.igdb.com",
     GAME_API_KEY: process.env.GAME_API_KEY,
-    IGDB_BASE_URL: process.env.IGDB_BASE_URL || "http://localhost:8080",
+    IGDB_BASE_URL: process.env.IGDB_BASE_URL || "https://hidden-hollows-79933.herokuapp.com",
 }
